b5affa5c7255e90cfd2738a52ab92896567d4323
0x464e <36742501+0x464e@users.noreply.github.com>
2022-12-16 23:44:17 +0200

    Revise eq tests

--- src/__tests__/eq.test.js
+++ src/__tests__/eq.test.js
@@ -5,7 +5,9 @@ describe('eq', () => {
         // Test that the function returns true for equivalent values
         expect(eq(1, 1)).toBe(true);
         expect(eq('hello', 'hello')).toBe(true);
-        expect(eq([1, 2, 3], [1, 2, 3])).toBe(true);
+        expect(eq(null, null)).toBe(true);
+        expect(eq(undefined, undefined)).toBe(true);
+        expect(eq(true, true)).toBe(true);
     });
 
     it('should return false for non-equivalent values', () => {
@@ -15,20 +17,25 @@ describe('eq', () => {
         expect(eq([1, 2, 3], [4, 5, 6])).toBe(false);
     });
 
+    it('should follow the ECMAScript Language Specification for equality comparisons with zero or NaN', () => {
+        // Test that the function follows the ECMAScript Language Specification for equality comparisons
+        expect(eq(0, -0)).toBe(true);
+        expect(eq(-0, 0)).toBe(true);
+        expect(eq(NaN, NaN)).toBe(true);
+        expect(eq(0, NaN)).toBe(false);
+        expect(eq(NaN, 0)).toBe(false);
+        expect(eq(-0, NaN)).toBe(false);
+        expect(eq(NaN, -0)).toBe(false);
+    });
+
     it('should return false for different types of equivalent values', () => {
         // Test that the function returns false for different types of equivalent values
+        // this is according to the ECMAScript Language Specification
         expect(eq(1, '1')).toBe(false);
         expect(eq(0, false)).toBe(false);
         expect(eq(1, true)).toBe(false);
         expect(eq('', false)).toBe(false);
         expect(eq('0', false)).toBe(false);
-    });
-
-    it('should handle NaN correctly', () => {
-        // Test that the function can handle NaN
-        expect(eq(NaN, NaN)).toBe(true);
-        expect(eq(NaN, 1)).toBe(false);
-        expect(eq(1, NaN)).toBe(false);
     });
 
     it('should handle objects correctly', () => {
